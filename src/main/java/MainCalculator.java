1e96a5eac77b7be902813e33085eb9f9ccbda8e3
Agata Ciesluk <agata.mikla@gmail.com>
2022-02-06 22:05:43 +0100

    Servlet created.

--- src/main/java/MainCalculator.java
+++ src/main/java/MainCalculator.java
@@ -10,6 +10,37 @@ public class MainCalculator extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String operation = req.getParameter("operation");
+        double val1 = Double.parseDouble(req.getParameter("val1"));
+        String val1Unit = req.getParameter("val1Unit");
+        double val2 = Double.parseDouble(req.getParameter("val2"));
+        String val2Unit = req.getParameter("val2Unit");
+        String resultUnit = req.getParameter("resultUnit");
+
+        if (val1 > 0 && val2 > 0) {
+            switch (operation) {
+                case "add":
+                    double sum = Calculator.addAndConvert(val1, val1Unit, val2, val2Unit, resultUnit);
+                    resp.getWriter().printf("Sum = %.2f %s", sum, resultUnit);
+                    break;
+                case "subtract":
+                    double difference = Calculator.subtractAndConvert(val1, val1Unit, val2, val2Unit, resultUnit);
+                    resp.getWriter().printf("Difference = %.2f %s", difference, resultUnit);
+                    break;
+                case "multiply":
+                    double product = Calculator.multiplyAndConvert(val1, val1Unit, val2, val2Unit, resultUnit);
+                    resp.getWriter().printf("Product = %.2f %s", product, resultUnit);
+                    break;
+                case "divide":
+                    double quotient = Calculator.divideAndConvert(val1, val1Unit, val2, val2Unit, resultUnit);
+                    resp.getWriter().printf("Quotient = %.2f %s", quotient, resultUnit);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Available options are: add, subtract, multiply, divide.");
+            }
+        } else {
+            throw new IllegalArgumentException("Expecting values greater than 0.00.");
+        }
 
     }
 }
--- src/main/java/MainCodeChecks.java
+++ src/main/java/MainCodeChecks.java
@@ -2,11 +2,11 @@ import org.apache.commons.math3.util.Precision;
 
 public class MainCodeChecks {
     public static void main(String[] args) {
-        System.out.println(Calculator.converter(8000,"m","NM"));
-        System.out.println(Calculator.converter(8000,"ft","NM"));
-        System.out.println("Sum = " + Calculator.addAndConvert(8000,"m",8000,"ft","NM"));
-        System.out.println("Difference = " + Calculator.subtractAndConvert(8000,"m",8000,"ft","NM"));
-        System.out.println("Product = " + Calculator.multiplyAndConvert(8000,"m",8000,"ft","NM"));
-        System.out.println("Quotient = " + Calculator.divideAndConvert(8000,"m",0,"ft","NM"));
+        System.out.println(Calculator.converter(8000,"ft","m"));
+        System.out.println(Calculator.converter(1,"NM","m"));
+        System.out.println("Sum = " + Calculator.addAndConvert(8000,"ft",1,"NM","m"));
+        System.out.println("Difference = " + Calculator.subtractAndConvert(8000,"ft",1,"NM","m"));
+        System.out.println("Product = " + Calculator.multiplyAndConvert(8000,"ft",1,"NM","m"));
+        System.out.println("Quotient = " + Calculator.divideAndConvert(8000,"ft",1,"NM","m"));
     }
 }
