520b56377aca80af51f3483897578ab9ce6b7308
Agata Ciesluk <agata.mikla@gmail.com>
2022-02-06 14:52:54 +0100

    Converter method - convert to meters.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,41 @@
+import java.math.BigDecimal;
+
+public class Calculator {
+
+    public static double addAndConvert (double val1, double val2, String val1Unit, String val2Unit, String resultUnit){
+        double sum = 0; //use Precision.round(sum,2) to get double value rounded to 2 decimal places
+
+
+        return sum;
+    }
+
+    public static double converter (double value, String valueUnit, String convertToUnit) {
+        double convertedValue = 0;
+
+        switch (convertToUnit) {
+            case "m":
+                switch (valueUnit) {
+                    case "m":
+                        convertedValue = value;
+                        break;
+                    case "NM":
+                        convertedValue = value * 1852;
+                        break;
+                    case "ft":
+                        convertedValue = value * 0.3048;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Available units are: m (meters), ft (feet), NM (nautical miles).");
+                }
+                break;
+            case "NM":
+                break;
+            case "ft":
+                break;
+            default:
+                throw new IllegalArgumentException("Available units are: m (meters), ft (feet), NM (nautical miles).");
+
+        }
+        return convertedValue;
+    }
+}
---
+++ src/main/java/MainCodeChecks.java
@@ -0,0 +1,8 @@
+import org.apache.commons.math3.util.Precision;
+
+public class MainCodeChecks {
+    public static void main(String[] args) {
+        System.out.println(Precision.round(Calculator.converter(17.00,"m","m"),2));
+
+    }
+}
